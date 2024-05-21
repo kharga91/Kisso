efbad1d1efe1c2af2970a39e7e6ef569c5f7a2fd
aniket kharga <aniketkharga91@gmail.com>
2024-05-21 08:44:54 +0545

    boxslice file made  to learn Redux

--- client/src/app/layout.js
+++ client/src/app/layout.js
@@ -1,5 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { NextUIProvider } from "@nextui-org/react";
+import ReduxProvider from "@/redux/ReduxProvider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -10,8 +12,10 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+    <NextUIProvider>
+  <ReduxProvider>
+      {children}
+      </ReduxProvider>
+    </NextUIProvider>
   );
 }
---
+++ client/src/redux/reducerSlices/boxSlice.jsx
@@ -0,0 +1,26 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = { 
+    backgroundColor: 'red'
+  width: 50,
+  height: 50,
+}
+
+const boxSlice = createSlice({
+  name: 'box',
+  initialState: initialState,
+  reducers: {
+    changeHeight(state) {
+      state.heightlue=state.height+1
+    },
+    decrement(state) {
+      state.value=state.value-1
+    },
+    reset(state){
+        state.value = 0
+    }
+  },
+})
+
+export const { changeHeight, decrement, reset } = boxSlice.actions
+export default boxSlice.reducer
